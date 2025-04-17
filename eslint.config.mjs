99415a7143877f1196c5baf02ba3654cafda0a05
Littlestar0508 <rist9860@gmail.com>
2025-04-17 16:29:09 +0900

    [INIT] : eslint 구성

---
+++ eslint.config.mjs
@@ -0,0 +1,40 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { FlatCompat } from "@eslint/eslintrc";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+  recommendedConfig: js.configs.recommended,
+});
+
+const eslintConfig = [
+  {
+    ignores: [".next"],
+    files: ["**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}"],
+    ...jsxA11y.flatConfigs.recommended,
+    plugins: {
+      "jsx-a11y": jsxA11y,
+    },
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.serviceworker,
+        ...globals.browser,
+      },
+    },
+  },
+  ...compat.config({
+    extends: ["eslint:recommended", "next/core-web-vitals", "next/typescript", "prettier"],
+    rules: {
+      "react/prop-types": "off",
+      "@next/next/no-img-element": "off",
+    },
+  }),
+];
+
+export default eslintConfig;
