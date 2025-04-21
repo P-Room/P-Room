cc9ae5cf1ff58067cc117858045a9f4797c9fd36
Namyejun <yejun1148@gmail.com>
2025-04-21 23:25:05 +0900

    [INIT] : 백엔드 초기 프로젝트 커밋

---
+++ P-Room/src/main/java/com/yejun/proom/PRoomApplication.java
@@ -0,0 +1,13 @@
+package com.yejun.proom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PRoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PRoomApplication.class, args);
+    }
+
+}
---
+++ P-Room/src/test/java/com/yejun/proom/PRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yejun.proom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PRoomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
