0560062596dc37a455bca14950f4c285ba6ba899
Littlestar0508 <rist9860@gmail.com>
2025-04-21 17:08:01 +0900

    [FEAT] : 속성으로 width를 받아와서 처리

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,19 +1,33 @@
+import { tm } from '@/utils/tw-merge'
+
 interface ButtonProps {
-  fill: boolean
+  fill?: boolean
+  wid?: number | string
 }
 
 function Button({
   children,
-  fill,
+  fill = true,
+  wid = 'fit',
 }: React.ComponentProps<'button'> & ButtonProps) {
   return (
     <>
       {fill ? (
-        <button className="flex justify-center items-center border text-white bg-primary border-primary py-2 px-4 rounded-2xl text-center cursor-pointer">
+        <button
+          className={tm(
+            'flex justify-center items-center border text-white bg-primary border-primary py-2 px-4 rounded-2xl text-center cursor-pointer',
+            `w-${wid}`
+          )}
+        >
           {children}
         </button>
       ) : (
-        <button className="flex justify-center items-center border border-primary py-2 px-4 rounded-2xl text-center cursor-pointer">
+        <button
+          className={tm(
+            'flex justify-center items-center border border-primary py-2 px-4 rounded-2xl text-center cursor-pointer',
+            `w-${wid}`
+          )}
+        >
           {children}
         </button>
       )}
