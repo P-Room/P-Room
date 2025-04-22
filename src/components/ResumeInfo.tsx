8004b0a0657731a7946769f3bd53251cc25dfef2
Littlestar0508 <rist9860@gmail.com>
2025-04-22 15:06:43 +0900

    [FEAT] : 이력서 정보를 속성으로 분류하여 css 작성
    - 요구 경력 dropdown은 구현 X
    - 기간에 캘린더는 구현X

--- src/components/ResumeInfo.tsx
+++ src/components/ResumeInfo.tsx
@@ -2,10 +2,29 @@ import { tm } from '@/utils/tw-merge'
 import Image from 'next/image'
 
 interface ResumeInfoProps {
-  sort: string
+  sort: '기업' | '공고 링크' | '직무' | '요구 경력' | '기간'
 }
 
 function ResumeInfo({ sort }: ResumeInfoProps) {
+  let src: string = 'company'
+  switch (sort) {
+    case '기업':
+      src = 'company.svg'
+      break
+    case '공고 링크':
+      src = 'link.svg'
+      break
+    case '직무':
+      src = 'duty.svg'
+      break
+    case '요구 경력':
+      src = 'require.svg'
+      break
+    case '기간':
+      src = 'duedate.svg'
+      break
+  }
+
   return (
     <div
       className={tm(
@@ -13,11 +32,15 @@ function ResumeInfo({ sort }: ResumeInfoProps) {
         'border-primary border-b'
       )}
     >
-      <Image src="/icons/company.svg" alt="회사" width={24} height={24} />
+      <Image src={`/icons/${src}`} alt={sort} width={24} height={24} />
       <label htmlFor={sort} className={tm('font-bold', 'w-25')}>
-        기업
+        {sort}
       </label>
-      <input id={sort}></input>
+      <input
+        id={sort}
+        placeholder={`${sort} 정보를 입력해주세요.`}
+        className={tm('py-1 px-1 w-full')}
+      />
     </div>
   )
 }
