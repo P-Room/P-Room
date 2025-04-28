c1886adf18d5221d16a7e61df07cd35c553721fa
Littlestar0508 <rist9860@gmail.com>
2025-04-28 15:56:11 +0900

    [FEAT] : 작성 글 목록 아이템 CSS 및 리스트 렌더링

---
+++ src/components/ResumeListItem.tsx
@@ -0,0 +1,45 @@
+import { tm } from '@/utils/tw-merge'
+
+interface ResumeListItemProps {
+  title: string
+  duedate: string[]
+  hashtag: string[]
+}
+
+function ResumeListItem({
+  title,
+  duedate,
+  hashtag,
+  id,
+}: ResumeListItemProps & React.ComponentProps<'div'>) {
+  return (
+    <div
+      className={tm(
+        'flex flex-col gap-6',
+        'w-3/5 border-2 border-primary rounded-2xl',
+        'mx-auto p-6'
+      )}
+    >
+      <button type="button" className="text-left text-2xl cursor-pointer">
+        {title}
+      </button>
+      <p className="font-bold">{`${duedate[0]} ~ ${duedate[1]}`}</p>
+      <div className="flex flex-row gap-2">
+        {hashtag.map((item, index) => (
+          <span
+            className={tm(
+              'bg-hashbg border-secondary border-1 rounded-full',
+              'px-3 py-1',
+              'text-secondary'
+            )}
+            key={item + index}
+          >
+            #{item}
+          </span>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default ResumeListItem
