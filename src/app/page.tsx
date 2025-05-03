eecf7a381b5b08f286f884c0ff9a0b63aee9de8e
Littlestar0508 <rist9860@gmail.com>
2025-05-03 13:19:43 +0900

    [FEAT] : 화면에 글쓰기 버튼 부착

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Calendar from '@/components/Calendar'
+import WriteButton from '@/components/WriteButton'
 
 export default function Page() {
   return (
     <main>
       <Calendar />
+      <WriteButton />
     </main>
   )
 }
