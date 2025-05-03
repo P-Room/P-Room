6a6b3eb3cd41d7689404aae539aebb286eec1f7d
Littlestar0508 <rist9860@gmail.com>
2025-05-03 13:54:03 +0900

    [FEAT] : 작성글 리스트 페이지 구현

---
+++ src/app/resume-list/page.tsx
@@ -0,0 +1,36 @@
+import ResumeListItem from '@/components/ResumeListItem'
+import { tm } from '@/utils/tw-merge'
+
+function ResumeList() {
+  return (
+    <>
+      <h1 className={tm('text-center font-bold text-3xl', 'mt-8 mb-12')}>
+        작성글 목록
+      </h1>
+      <div className={tm('flex flex-col gap-6 mb-10')}>
+        <ResumeListItem
+          title="제목 1"
+          duedate={['2025-05-01', '2025-05-03']}
+          hashtag={['행복', '소망', '사랑']}
+        />
+        <ResumeListItem
+          title="제목 2"
+          duedate={['2025-05-01', '2025-05-03']}
+          hashtag={['행복', '소망', '사랑']}
+        />
+        <ResumeListItem
+          title="제목 3"
+          duedate={['2025-05-01', '2025-05-03']}
+          hashtag={['행복', '소망', '사랑']}
+        />
+        <ResumeListItem
+          title="제목 4"
+          duedate={['2025-05-01', '2025-05-03']}
+          hashtag={['행복', '소망', '사랑']}
+        />
+      </div>
+    </>
+  )
+}
+
+export default ResumeList
