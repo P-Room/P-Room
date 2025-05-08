f05a9b04bc22d467b881884d52560036a0adcc0e
Littlestar0508 <rist9860@gmail.com>
2025-05-08 15:58:57 +0900

    [STYLE] : 모바일 반응형 스타일링을 헤더에 적용(큰 틀)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ function Header() {
       <div
         className={tm(
           'flex flex-row justify-between items-center',
-          'w-full h-16 py-2 pl-4 pr-8',
+          'md:w-full h-16 py-2 pl-4 pr-8',
           'shadow-[0_2px_4px_rgba(0,0,0,0.25)]',
           // 추후 삭제
           'mb-4'
