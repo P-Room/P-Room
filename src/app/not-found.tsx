4980fb9a83b8a05db2e494d109cd821e5adaefaf
Littlestar0508 <rist9860@gmail.com>
2025-05-15 14:34:25 +0900

    [FEAT] : 404페이지 디자인 변경 및 메인 페이지로 유도

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,9 +2,17 @@ import Link from 'next/link'
 
 function NotFound() {
   return (
-    <div>
-      <h1>Not Found - 404!</h1>
-      <Link href="/">Go Back To Home</Link>
+    <div className="w-4/5 flex flex-col items-center gap-2 absolute top-[50%] translate-y-[-50%] left-[50%] translate-x-[-50%]">
+      <h1 className="font-bold text-secondary text-3xl md:text-5xl mb-10">
+        404 ERROR
+      </h1>
+      <p>죄송합니다. 페이지를 찾을 수 없습니다.</p>
+      <p>존재하지 않는 주소를 입력하셨거나,</p>
+      <p>요청하신 페이지의 주소가 변경,삭제되어 찾을 수 없습니다.</p>
+      <span className="w-1/2 bg-secondary h-50 text-center my-5">로고</span>
+      <Link href="/" className="underline hover:text-primary my-4">
+        홈으로
+      </Link>
     </div>
   )
 }
