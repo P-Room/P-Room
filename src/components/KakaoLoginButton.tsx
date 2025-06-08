921d52879cdb604a0ae73473e33b61772d1e45d6
Littlestar0508 <rist9860@gmail.com>
2025-06-08 17:16:01 +0900

    [FEAT] : 카카오 로그인 백엔드 연동 테스트

--- src/components/KakaoLoginButton.tsx
+++ src/components/KakaoLoginButton.tsx
@@ -4,13 +4,13 @@ import { tm } from '@/utils/tw-merge'
 import Image from 'next/image'
 
 function KakaoLoginButton() {
-  // 키 값과 URI는 이후 개발 과정에서 추가될 예정
-  const REST_API_KEY = '로그인에사용될키값'
-  const REDIRECT_URI = '로그인후에사용될값'
-  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`
-
-  // 로그인 시 카카오로 연결해주는 역할을 담당(SPA적 움직임X)
   const handleLogin = () => {
+    // 키 값과 URI는 이후 개발 과정에서 추가될 예정
+    const REST_API_KEY = 'd773ffb4016733284e32a07069ebf666'
+    const REDIRECT_URI = 'http://43.202.53.55:8080/login/oauth2/code/kakao'
+    const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}`
+
+    // 로그인 시 카카오로 연결해주는 역할을 담당(SPA적 움직임X)
     window.location.href = KAKAO_AUTH_URL
   }
 
