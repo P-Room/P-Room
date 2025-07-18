73979af56fa61750203cb1d7dc15ed438c03c375
Namyejun <yejun1148@gmail.com>
2025-07-18 18:20:38 +0900

    [MODIFY] : me api return user's email

--- P-Room/src/main/java/com/yejun/proom/controller/PersonController.java
+++ P-Room/src/main/java/com/yejun/proom/controller/PersonController.java
@@ -2,16 +2,22 @@ package com.yejun.proom.controller;
 
 import com.yejun.proom.util.CustomOAuth2User;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 public class PersonController {
     @GetMapping("/me")
-    public String getPerson(@AuthenticationPrincipal CustomOAuth2User oAuth2User, HttpSession session) {
-        System.out.println("! SESSION ID = " + session.getId() + oAuth2User.getName());
-        return oAuth2User.getPerson().getEmail();
+    public ResponseEntity<?> getPerson(@AuthenticationPrincipal CustomOAuth2User user) {
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build(); // 👈 이렇게!
+        }
+        return ResponseEntity.ok(Map.of("email", user.getPerson().getEmail()));
     }
 }
